5cb217869a28501cd58469b7ed4c1293f85ffe58
Ischca <03.suiseiseki@gmail.com>
2020-12-22 13:10:06 +0900

    足し算と引き算を追加

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::process;
 use std::env;
+use libc::strtol;
+use std::ffi::CString;
+use std::os::raw::c_char;
+use std::ptr;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
@@ -9,9 +13,29 @@ fn main() {
     process::exit(1);
   }
 
+  let mut p = CString::new(args[1].clone()).expect("error").into_raw();
+
   println!(".intel_syntax noprefix");
   println!(".globl main");
   println!("main:");
-  println!("  mov rax, {}", args[1]);
+  unsafe {
+    println!("  mov rax, {}", strtol(p, &mut p as *mut *mut i8, 10));
+
+    while *p as u8 != b'\0' {
+      let ptr = *p as u8 as char;
+      match ptr {
+        '+' => {
+          p = p.offset(1);
+          println!("  add rax, {}", strtol(p, &mut p as *mut *mut i8, 10));
+        },
+        '-' => {
+          p = p.offset(1);
+          println!("  sub rax, {}", strtol(p, &mut p as *mut *mut i8, 10));
+        },
+        _ => panic!("予期しない文字です: {}", *p),
+      }
+    }
+  }
+  
   println!("  ret");
 }
